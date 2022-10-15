c48e707f3afd5f57d960436f0fa3b1402f534261
Naza <uzoukwuc@tcd.ie>
2022-10-15 22:47:55 +0100

    I now distinguish between starting characters and ending characters so that code is somewhat readable. Also moved examples to a folder as maintaining them in two places is pain

--- README.md
+++ README.md
@@ -10,17 +10,17 @@ Regular text is in a literal section. It is more or less printed as is, unless y
 
 Inside a literal section, the following syntax is recognised.
 
-* `% FUNCTION ARGS %`: Evaluates an expression silently
-* `%~ FUNCTION ARGS %`: the same as `%`, but the output is printed out.
-* `%= VARIABLE %`: prints out the value of a variable
-* `%=~ FORMAT-STRING VARIABLE* %`: prints out the value of a variable and allows you to specify a format string to use
+* `[% FUNCTION ARGS %]`: Evaluates an expression silently
+* `[%~ FUNCTION ARGS %]`: the same as `%]`, but the output is printed out.
+* `[%= VARIABLE %]`: prints out the value of a variable
+* `[%=~ FORMAT-STRING VARIABLE* %]`: prints out the value of a variable and allows you to specify a format string to use
 
 Each of these start a statement.
 Inside a statement the following syntax is recognised:
 
-* `%`: The character that ends the statement
-* `@ TEXT @`: Starts a nested literal section.
-Literal sections are ended by the `@` character.
+* `%]`: The token that ends the statement
+* `[@ TEXT @]`: Starts a nested literal section.
+Literal sections are ended by the `@]` token.
 
 ## Usage 
 
@@ -32,33 +32,4 @@ echo '%= "Hello World %' | intralisp
 
 ## Examples
 
-### Basic
-```
-%loop for i from 1 to 10 do
-@Hello for the %=~"~:r"i% time!
-@%
-```
-
-### VHDL 😏
-```
-entity Adder3Bit_TB is
-end 
-
-...
-
-% dotimes (i 8)
-(dotimes (j 8) @
-wait until falling_edge(CLK);
-A_TB <= %=~ "~3,'0b" i%;
-B_TB <= %=~ "~3,'0b" j%;
-@)%
-```
-
-### Random Numbers
-```
-Let's generate some (pseudo) random numbers:
-% setf *random-state* (make-random-state t) %
-% dotimes ( i 10 )
-@* Here is the %=~"~:R"i% random number: %~ random 8 %
-@ %
-```
+See examples folder.
--- intralisp.lisp
+++ intralisp.lisp
@@ -1,21 +1,21 @@
-(defvar start-statement "%")
-(defvar end-statement "%")
-(defvar start-literal "@")
-(defvar end-literal "@")
+(defvar start-statement "[%")
+(defvar end-statement "%]")
+(defvar start-literal "[@")
+(defvar end-literal "@]")
 
 (defun intralisp-print-literal (literal)
   (unless (= 0 (length literal))
     (format t "~s~%" `(format t "~a" ,literal))))
 
 (defun intralisp-parse-statement (rest &key (accumulated-length 0) (accumulated-string "") (terminator ")"))
-  (cond ((equal start-literal (subseq rest 0 1))
+  (cond ((equal start-literal (subseq rest 0 (length start-literal)))
          (format t "~a~%" accumulated-string)
-         (let ((literal-length (+ 2 (intralisp-parse-literal (subseq rest 1)))))
+         (let ((literal-length (+ 4 (intralisp-parse-literal (subseq rest (length start-literal))))))
            (intralisp-parse-statement
             (subseq rest literal-length)
             :terminator terminator
             :accumulated-length (+ accumulated-length literal-length (length accumulated-string)))))
-        ((equal end-statement (subseq rest 0 1))
+        ((equal end-statement (subseq rest 0 (length end-statement)))
          (format t "~a~a~%" accumulated-string terminator)
          (+ accumulated-length (length accumulated-string)))
         (t
@@ -26,17 +26,17 @@
           :accumulated-string (concatenate 'string accumulated-string (subseq rest 0 1))))))
 
 (defun intralisp-parse-literal (rest &key (accumulated-length 0) (accumulated-string "") )
-  (cond ((and (<= 1 (length rest))
-              (equal start-statement (subseq rest 0 1)))
+  (cond ((and (<= (length start-literal) (length rest))
+              (equal start-statement (subseq rest 0 (length start-statement))))
          (intralisp-print-literal accumulated-string)
          (multiple-value-bind (start-delimiter-length
                                end-delimiter-length
                                implicit-predicate
                                terminator)
-             (cond ((equal "=~" (subseq rest 1 3))  (values 3 1 "(FORMAT T " ")"))
-                   ((equal #\~ (aref rest 1)) (values 2 1 "(FORMAT T \"~a\" (" "))"))
-                   ((equal #\= (aref rest 1)) (values 2 1 "(FORMAT T \"~a\" " ")"))
-                   (t (values 1 1 "(" ")")))
+             (cond ((equal "=~" (subseq rest (length start-statement) (+ 2 (length start-statement))))  (values 4 2 "(FORMAT T " ")"))
+                   ((equal #\~ (aref rest (length start-statement))) (values 3 2 "(FORMAT T \"~a\" (" "))"))
+                   ((equal #\= (aref rest (length start-statement))) (values 3 2 "(FORMAT T \"~a\" " ")"))
+                   (t (values 2 2 "(" ")")))
            (format t "~a" implicit-predicate)
            (let ((statement-length (+ start-delimiter-length
                                       (intralisp-parse-statement (subseq rest start-delimiter-length)
@@ -46,7 +46,8 @@
               (subseq rest statement-length)
               :accumulated-length (+ accumulated-length statement-length (length accumulated-string))))))
         ((or (= 0 (length rest))
-             (equal end-literal (subseq rest 0 1)))
+             (and (<= (length end-literal) (length rest))
+                  (equal end-literal (subseq rest 0 (length end-literal)))))
          (intralisp-print-literal accumulated-string)
          (+ accumulated-length (length accumulated-string)))
         (t

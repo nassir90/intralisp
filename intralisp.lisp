b0f0ccbde05ffe1dd33844eb8273c03b2cf9b992
Naza <uzoukwuc@tcd.ie>
2022-10-31 13:06:23 +0000

    Use with-output-to-string with *standard-output* as output breaks accross modules

--- intralisp.lisp
+++ intralisp.lisp
@@ -11,24 +11,23 @@
 
 (defvar loaded-files nil)
 
-(defun print-literal (stream literal &key (terminal nil))
+(defun print-literal (literal &key (terminal nil))
   (unless (or (= 0 (length literal))
               (and (= 1 (length literal))
                    (equal #\newline (aref literal 0))
                    (not terminal)))
-    (format T "~s~%" `(format ,(intern stream) "~a" ,literal))))
+    (format T "~s~%" `(format T  "~a" ,literal))))
 
-(defun parse-statement (stream rest &key (accumulated-length 0) (accumulated-string "") (terminator ")"))
+(defun parse-statement (rest &key (accumulated-length 0) (accumulated-string "") (terminator ")"))
   (cond ((equal start-literal (subseq rest 0 (length start-literal)))
          (format t "~a~%" accumulated-string)
-         (multiple-value-bind (start-delimiter-length implicit-predicate literal-terminator stream)
-             (cond ((equal (aref rest (length start-literal)) #\^) (values (+ 1 (length start-literal)) "(WITH-OUTPUT-TO-STRING (OUTPUT)~%" ")" "OUTPUT"))
-                   (t (values (length start-literal) "" "" "T")))
+         (multiple-value-bind (start-delimiter-length implicit-predicate literal-terminator)
+             (cond ((equal (aref rest (length start-literal)) #\^) (values (+ 1 (length start-literal)) "(WITH-OUTPUT-TO-STRING (*standard-output*)~%" ")"))
+                   (t (values (length start-literal) "" "")))
            (format t implicit-predicate)
-           (let* ((literal-length (+ start-delimiter-length (length end-literal) (parse-literal stream (subseq rest start-delimiter-length)))))
+           (let* ((literal-length (+ start-delimiter-length (length end-literal) (parse-literal (subseq rest start-delimiter-length)))))
              (format t literal-terminator)
              (parse-statement
-              stream
               (subseq rest literal-length)
               :terminator terminator
               :accumulated-length (+ accumulated-length literal-length (length accumulated-string))))))
@@ -37,17 +36,16 @@
          (+ accumulated-length (length accumulated-string)))
         (t
          (parse-statement
-          stream
           (subseq rest 1)
           :terminator terminator
           :accumulated-length accumulated-length
           :accumulated-string (concatenate 'string accumulated-string (subseq rest 0 1))))))
 
-(defun parse-literal (stream rest &key (accumulated-length 0) (accumulated-string ""))
+(defun parse-literal (rest &key (accumulated-length 0) (accumulated-string ""))
   (cond ((and (<= (1+ (length start-literal)) (length rest))
               (equal start-statement (subseq rest 0 (length start-statement)))
               (equal #\! (aref rest (length start-statement))))
-         (print-literal stream accumulated-string)
+         (print-literal accumulated-string)
          (let* ((after-first-quote (1+ (position #\" rest)))
                 (second-quote (position #\" rest :start after-first-quote))
                 (end-of-section (+ (length end-statement) (search end-statement rest)))
@@ -58,47 +56,42 @@
                (parse-stream stream))
              (format t "(in-package :cl-user)~%"))
            (parse-literal
-            stream
             (subseq rest end-of-section)
             :accumulated-length (+ accumulated-length end-of-section))))
         ((and (<= (length start-literal) (length rest))
              (equal start-statement (subseq rest 0 (length start-statement))))
-         (print-literal stream accumulated-string)
+         (print-literal accumulated-string)
          (multiple-value-bind (start-delimiter-length
                                implicit-predicate
                                terminator)
              (cond ((equal "=~" (subseq rest (length start-statement) (+ 2 (length start-statement))))
-                    (values 4 (concatenate 'string "(FORMAT " stream " ") ")"))
+                    (values 4 "(FORMAT T " ")"))
                    ((equal #\~ (aref rest (length start-statement)))
-                    (values (+ 1 (length start-statement)) (concatenate 'string "(FORMAT " stream " \"~a\" (") "))"))
+                    (values (+ 1 (length start-statement)) "(FORMAT T \"~a\" (" "))"))
                    ((equal #\= (aref rest (length start-statement)))
-                    (values (+ 1 (length start-statement)) (concatenate 'string "(FORMAT " stream  " \"~a\" ") ")"))
+                    (values (+ 1 (length start-statement)) "(FORMAT T \"~a\" " ")"))
                    (t (values (length start-statement) "(" ")")))
            (format t "~a" implicit-predicate)
            (let ((statement-length (+ start-delimiter-length
-                                      (parse-statement stream
-                                                       (subseq rest start-delimiter-length)
+                                      (parse-statement (subseq rest start-delimiter-length)
                                                        :terminator terminator)
                                       (length end-statement))))
              (parse-literal
-              stream
               (subseq rest statement-length)
               :accumulated-length (+ accumulated-length statement-length (length accumulated-string))))))
         ((or (= 0 (length rest))
              (and (<= (length end-literal) (length rest))
                   (equal end-literal (subseq rest 0 (length end-literal)))))
-         (print-literal stream accumulated-string :terminal t)
+         (print-literal accumulated-string :terminal t)
          (+ accumulated-length (length accumulated-string)))
         (t
          (parse-literal
-          stream
           (subseq rest 1)
           :accumulated-length accumulated-length
           :accumulated-string (concatenate 'string accumulated-string (subseq rest 0 1))))))
 
 (defun parse-stream (stream)
   (parse-literal
-   "T"
    (reduce (lambda (a b)
              (concatenate 'string a b))
            (loop
